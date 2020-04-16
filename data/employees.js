4d120e06727f932db6e330a0cfeb9a775ee5b498
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-04-16 14:58:50 +0200

    Update company id not to be 0

--- data/employees.js
+++ data/employees.js
@@ -4,10 +4,12 @@ module.exports = function (amount) {
   var entities = [];
 
   for (var id = 1; id <= amount; id++) {
+    var companyId = faker.random.number(3);
+    if (companyId === 0) companyId = 1;
     let entity = {
       id: id,
       avatar: faker.image.avatar(),
-      company_id: faker.random.number(3),
+      company_id: companyId,
     };
     Object.assign(entity, faker.helpers.createCard());
     entities.push(entity);
